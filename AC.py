51d48a30bf80642069e12bacd9893167d8267174
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2022-12-04 19:55:11 +0700

    change smt

--- AC.py
+++ AC.py
@@ -7,8 +7,8 @@ randomrange = random.randrange(0, len(data))
 
 class AC:
 
-    def __init__(self, name='', temp='', mode='',
-                 fan=['0', 'OFF'], status='close'):
+    def __init__(self, name='', temp='', mode='', fan=['0', 'OFF'],
+                 status='close'):
         self.name = name
         self.temp = temp
         self.mode = mode
@@ -18,12 +18,15 @@ class AC:
     def open_close(self, oc):
         if oc == 'open':
             self.status = oc
+            self.temp = '25'
+            self.mode = 'Cool'
+            self.fan[0] = '1'
         if oc == 'close':
             self.status = oc
-            self.temp = ''
-            self.mode = ''
+            self.temp = '-'
+            self.mode = '-'
             self.fan[0] = '0'
-            self.fan[1] = ''
+            self.fan[1] = 'OFF'
 
     def swing(self, swing):
         if swing == 'ON' or swing == 'OFF':
@@ -35,19 +38,19 @@ class AC:
         else:
             print('speed must be greater than 0 and less than 6.')
 
-    def mode(self, index):
-        ac_mode = ['auto', 'cool', 'eco', 'heat', 'fan']
-        self.mode = ac_mode[index - 1]
-
     def temperature(self, temper):
-        if self.status == 'open' and self.temp != 'auto':
+        if self.mode != 'Auto':
             if 15 <= temper <= 30:
                 self.temp = str(temper)
             else:
                 print('temperature must be greater than 14 and less than 31.')
 
+    def mode(self, index):
+        ac_mode = ['Auto', 'Cool', 'Dry', 'Eco', 'Heat', 'Fan']
+        self.mode = ac_mode[index - 1]
+
     def auto(self):
-        if self.status == 'open' and self.mode == 'auto':
+        if self.mode == 'Auto':
             if data[randomrange]['season'] == 'summer':
                 if 28 <= int(data[randomrange]['temp']) <= 32:
                     self.temp = '22'
@@ -65,5 +68,5 @@ class AC:
                     self.temp = '26'
 
     def __str__(self):
-        return f'{self.name}-Status:{self.status} Temperature:{self.temp}C ' \
+        return f'{self.name}-Status:{self.status} Temperature:{self.temp}°C ' \
                f'Mode:{self.mode} Speed:{self.fan[0]} Swing:{self.fan[1]}'
