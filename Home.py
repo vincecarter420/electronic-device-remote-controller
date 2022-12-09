bc78c479f334821764199967794c08eaa3477f9a
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2022-12-09 14:39:09 +0700

    write a comment

--- Home.py
+++ Home.py
@@ -1,8 +1,10 @@
 class Home:
     def __init__(self, devices):
+        '''create devices value'''
         self.devices = devices
 
     def show_status(self):
+        '''show all device status'''
         for device in self.devices:
             print('')
             print(device)
